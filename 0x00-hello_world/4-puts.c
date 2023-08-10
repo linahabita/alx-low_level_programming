45f3684b9309c3697209c4540b5363ed32060004
lina <habita.lina@univ-ouargla.dz>
2023-08-10 10:40:10 -0700

    yes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * description: a c programe that print with function 
+ *
+ * return : always 0
+ */
+
+
+
+
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);	
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/** 
+ * main - entry point 
+ *
+ * description: A C programe that prints with printf function
+ *
+ * return : alwas 0 (success) 
+*/
+
+int main () 
+{
+       printf(" with proper grammar, but the outcome is a piece of art,");
+       return 0;
+}
