51766e795ff5690209680459de873c2fec1ad4b7
lina <habita.lina@univ-ouargla.dz>
2023-09-21 07:44:21 -0700

    k

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,34 +2,16 @@
 #include <string.h>
 #include "lists.h"
 
-
 /**
-* _strlen - finds the length of a string
-* @str: string to find the length of
-*
-* Return: length of string
-*/
-unsigned int _strlen(char *str)
-{
-   unsigned int i;
-
-
-   for (i = 0; str[i]; i++)
-       ;
-   return (i);
-}
-
-
-/**
-* add_node_end - adds a new node to the end of linked list
-* @head: double pointer to a linked list
-* @str: string to add to the new node
-*
-* Return: pointer to the new node
-*/
+ * add_node_end - adds a new node at the end of a linked list
+ * @head: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-   list_t *new;
+	list_t *new;
 	list_t *temp = *head;
 	unsigned int len = 0;
 
@@ -57,4 +39,3 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	return (new);
 }
-
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
-
 /**
-* free_list - Realease the memory allocated for a list
-*
-* @head: A pointer to the first node of the list to free
-*/
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
 void free_list(list_t *head)
 {
-  list_t *temp;
+	list_t *temp;
 
 	while (head)
 	{
