b3d8c919ae5e7d58a3ab5b7a440832f3ae0c8db7
lina <habita.lina@univ-ouargla.dz>
2023-09-25 15:45:53 -0700

    ok

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,16 @@
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
-{ 
-   size_t num = 0;
-       while(h){
-       printf("%d\n",h->n);
-       num++;
-       h=h->next;
-       }	
-     return (num);
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
+
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,17 +8,19 @@
  * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
-{ 
-    listint_t *new;
-    listint_t *temp = *head;
+{
+	listint_t *new;
+	listint_t *temp = *head;
 
-   new = malloc(sizeof(listint_t));
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 
-   if(!new)
-	   return NULL;
-   new->n=n;
-   new->next=NULL;
-   if(*head==NULL){
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
 		*head = new;
 		return (new);
 	}
@@ -30,5 +32,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	return (new);
 }
-
 
